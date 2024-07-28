24505c3a22b6e452cce9d82534ea3d179f91bdc4
XGaming Systems <travisisveryrudeonhere@gmail.ccom>
2024-07-28 20:21:27 +1000

    i think i am merging lol

---
+++ src/main/java/sidjack/sidjacksfurnituremod/SiDjacksFurnitureModClient.java
@@ -0,0 +1,10 @@
+package sidjack.sidjacksfurnituremod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SiDjacksFurnitureModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}

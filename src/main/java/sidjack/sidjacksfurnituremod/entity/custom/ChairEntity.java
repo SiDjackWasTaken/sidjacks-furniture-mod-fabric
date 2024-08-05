6552b21f0626926bfaa088f7e704f871363132b9
XGaming Systems <travisisveryrudeonhere@gmail.ccom>
2024-08-05 15:11:49 +1000

    i did something by accident lol

--- src/main/java/sidjack/sidjacksfurnituremod/entity/custom/ChairEntity.java
+++ src/main/java/sidjack/sidjacksfurnituremod/entity/custom/ChairEntity.java
@@ -1,4 +1,32 @@
 package sidjack.sidjacksfurnituremod.entity.custom;
 
-public class ChairEntity {
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.world.World;
+
+
+public class ChairEntity extends Entity {
+    public ChairEntity(EntityType<?> type, World world) {
+        super(type, world);
+        this.noClip = true;
+    }
+
+    @Override
+    protected void initDataTracker(DataTracker.Builder builder) {
+
+    }
+
+    @Override
+    protected void readCustomDataFromNbt(NbtCompound nbt) {
+
+    }
+
+    @Override
+    protected void writeCustomDataToNbt(NbtCompound nbt) {
+
+    }
 }
+
+
